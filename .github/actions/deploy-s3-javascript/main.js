36d509be4bd1f4e6d4936e3366c3e40619735afd
i044184 <matan.elitzur@sap.com>
2022-12-18 16:27:37 +0200

    upload to aws

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,7 +2,28 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
 function run() {
-    //This will print the following string in the action log in github
+    //1. Here we get the inputs from the deploy-s3-javascript/action.yml
+    const backet = core.getInput('backet', { required: true })
+    const backetRegion = core.getInput('backet-region', { required: true })
+    const distFolder = core.getInput('dist-folder', { required: true })
+    //2.
+    exec.exec('echo "Hello world"')
+    //2.5 we can find aws cli in the ubuntu, CLI Tools section https://github.com/actions/runner-images/blob/main/images/linux/Ubuntu2204-Readme.md#cli-tools
+    //so we can connect aws
+    const s3uri = `s3://${bucket}`
+    //exec.exec('aws s3 async ${distFolder} ${s3uri} --region ${backetRegion}')
+    //3.
+    console.log(`backet: ${backet}`)
+    console.log(`backetRegion: ${backetRegion}`)
+    console.log(`distFolder: ${distFolder}`)
+    
+    //4. github we are not using it but it's very usefull
+    //It is easy to send github requests to the github rest api
+    //github.getOctokit().rest
+    //Get some of the values of the github context 
+    //github.context.action //for example get the name of the action
+    
+    //5. This will print the following string in the action log in github
     core.notice('Hello from my custom JavaScript Action!')
 }
 
